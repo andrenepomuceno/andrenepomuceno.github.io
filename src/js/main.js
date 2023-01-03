7296abdd24b810e3166049b6110735f75758f215
Andre Nepomuceno <andrerm2k@hotmail.com>
2023-01-03 15:56:52 -0300

    index improvements

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,13 @@
 import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap'
 
-function analytics() {
-    window.dataLayer = window.dataLayer || [];
-    function gtag() { dataLayer.push(arguments); }
-    gtag('js', new Date());
-    gtag('config', 'G-8X67LC7LY9');
+if (ENV?.prod == true) {
+    function analytics() {
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+        gtag('config', 'G-8X67LC7LY9');
+    }
+    
+    analytics();
 }
-
-analytics();
