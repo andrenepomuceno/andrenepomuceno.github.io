42f48bed13ff29829c9fb0135a053af54e3420f2
Andre Nepomuceno <andrerm2k@hotmail.com>
2023-01-03 15:25:28 -0300

    Code cleanup

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,11 @@
 import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap'
+
+function analytics() {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-8X67LC7LY9');
+}
+
+analytics();
